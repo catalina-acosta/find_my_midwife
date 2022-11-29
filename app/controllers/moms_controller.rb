f6909c65d45a7710abe3a39ca13ee50596a39219
Joshua Smith <jmscodingde@gmail.com>
2022-11-29 12:31:19 +0100

    mom controller and routes

---
+++ app/controllers/moms_controller.rb
@@ -0,0 +1,43 @@
+class MomsController < ApplicationController
+  before_action :set_mom, only: []
+  before_action :set_bookmark, only: []
+
+  def index
+    @moms = Mom.all
+  end
+
+  def show; end
+
+  def new
+    @mom = Mom.new
+  end
+
+  def create
+    @mom = Mom.new(mom_params)
+    @mom.list = @list
+    if @mom.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @mom.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_list
+    @midwife = Midwife.find(params[:list_id])
+  end
+
+  def set_mom
+    @mom = Mom.find(params[:id])
+  end
+
+  def mom_params
+    params.require(:mom).permit(:due_date, :phone, :user_id)
+  end
+end
