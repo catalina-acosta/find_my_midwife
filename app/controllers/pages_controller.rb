eea32504f0d3c4a517cbf9fb88e530c63379d856
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2022-11-30 12:08:58 +0100

    more work on the index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
   def home
     return redirect_to new_user_session_path unless current_user
-    if current_user.midwife.present?
+    if current_user.midwife
       midwives_path
-    elsif current_user.mom.present?
+    elsif current_user.mom
       moms_path
     else
       new_mom_midwife_path
