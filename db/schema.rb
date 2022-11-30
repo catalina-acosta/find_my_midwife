197c4bbf0a897056bafe09f3da7f485beda06c8f
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2022-11-30 17:35:11 +0100

    navbar complete

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_171627) do
 
   create_table "appointments", force: :cascade do |t|
     t.string "title"
-    t.datetime "start_date"
+    t.datetime "start_time"
     t.bigint "booking_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
