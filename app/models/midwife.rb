9a88fad0aefff30b245cf7f58e514609153dd8c4
Joshua Smith <jmscodingde@gmail.com>
2022-11-30 18:18:23 +0100

    latest appointment works

--- app/models/midwife.rb
+++ app/models/midwife.rb
@@ -2,4 +2,9 @@ class Midwife < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :appointments, through: :bookings
+  delegate :first_name, :last_name, to: :user
+  
+  def upcoming_appointment
+    appointments.where("start_time > ?", DateTime.now).first
+  end
 end
--- app/models/mom.rb
+++ app/models/mom.rb
@@ -1,4 +1,11 @@
 class Mom < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many :appointments, through: :bookings
+  delegate :first_name, :last_name, to: :user
+
+
+  def upcoming_appointment
+    appointments.where("start_time > ?", DateTime.now).first
+  end
 end
