87c3ea8adf27c0679189e18ec79e4c44491b5dfd
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2022-12-05 12:54:41 +0100

    chat working, half way through stimulus

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_151913) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_084608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_151913) do
     t.index ["mom_id"], name: "index_bookings_on_mom_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "booking_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_messages_on_booking_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "midwives", force: :cascade do |t|
     t.string "speciality"
     t.integer "years_experience"
@@ -103,6 +113,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_151913) do
   add_foreign_key "appointments", "bookings"
   add_foreign_key "bookings", "midwives"
   add_foreign_key "bookings", "moms"
+  add_foreign_key "messages", "bookings"
+  add_foreign_key "messages", "users"
   add_foreign_key "midwives", "users"
   add_foreign_key "moms", "users"
 end
