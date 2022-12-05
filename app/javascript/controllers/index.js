de3fb932920b0ec0f4d360f118d24119b42d9f7d
Joshua Smith <jmscodingde@gmail.com>
2022-12-05 12:54:25 +0100

    Filter works

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import FilterMidwivesController from "./filter_midwives_controller"
+application.register("filter-midwives", FilterMidwivesController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
